95707a7fbdb5c87276a235207d2b8947742760f7
Sandro Santos <sandro.dev@gmail.com>
2020-02-08 11:11:01 -0300

    dotenv library, configuration and import in files

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,10 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ ok: true, message: 'The server ir running on port 3333' });
+  return res.json({
+    ok: true,
+    message: `The server ir running on port ${process.env.APP_PORT}`,
+  });
 });
 
 export default routes;
