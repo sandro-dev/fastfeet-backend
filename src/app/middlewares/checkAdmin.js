7f3be5bd39a8bbfab3606e5d6fb485de023f6508
Sandro Santos <sandro.dev@gmail.com>
2020-02-08 17:18:11 -0300

    authmiddleware to verify user token and checkAdmin middlware to verify if user is admin

---
+++ src/app/middlewares/checkAdmin.js
@@ -0,0 +1,14 @@
+import User from '../models/User';
+
+export default async (req, res, next) => {
+  const user = await User.findOne({
+    where: { id: req.userId, is_admin: true },
+  });
+
+  if (!user) {
+    return res
+      .status(401)
+      .json({ error: 'Only administrator user can access' });
+  }
+  return next();
+};
