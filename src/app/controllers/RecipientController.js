2fe67e264ff7f21e0e622d9f542c2a8cee65a39d
Sandro Santos <sandro.dev@gmail.com>
2020-02-08 17:18:35 -0300

    RecipientController and new routes

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,78 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async index(req, res) {
+    const recipients = await Recipient.findAll();
+    return res.json(recipients);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.string(),
+      complement: Yup.string(),
+      city: Yup.string().required(),
+      state: Yup.string().required(),
+      postcode: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const recipient = await Recipient.create(req.body);
+
+    return res.json({ ok: true, recipient });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.string(),
+      complement: Yup.string(),
+      city: Yup.string(),
+      state: Yup.string(),
+      postcode: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+
+    const recipient = await Recipient.findOne({
+      where: { id },
+    });
+
+    const { street, number, complement, city, state, postcode } = req.body;
+
+    recipient.street = street;
+    recipient.number = number;
+    recipient.complement = complement;
+    recipient.city = city;
+    recipient.state = state;
+    recipient.postcode = postcode;
+
+    recipient.save();
+
+    return res.json({ ok: true, recipient });
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const recipient = await Recipient.destroy({
+      where: { id },
+    });
+    if (!recipient) {
+      return res.json({ error: 'Error on trying to delete recipient' });
+    }
+    return res.json({ ok: true, message: 'The recipient was deleted' });
+  }
+}
+
+export default new RecipientController();
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+import checkAdminMiddleware from './app/middlewares/checkAdmin';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 
 const routes = new Router();
 
@@ -13,10 +17,20 @@ routes.get('/', (req, res) => {
 });
 
 routes.get('/users', UserController.index);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
 routes.post('/users', UserController.store);
 routes.put('/users/:id', UserController.update);
+
+routes.use(checkAdminMiddleware);
+
 routes.delete('/users/:id', UserController.delete);
 
-routes.post('/sessions', SessionController.store);
+routes.get('/recipients', RecipientController.index);
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
+routes.delete('/recipients/:id', RecipientController.delete);
 
 export default routes;
