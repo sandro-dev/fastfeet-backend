fc4cc157e1f5086aed6a8ec0b99bbb8caed6401b
Sandro Santos <sandro.dev@gmail.com>
2020-02-09 12:58:33 -0300

    deliveries and signatures migrations

---
+++ src/database/migrations/20200209155551-create-deliveries.js
@@ -0,0 +1,70 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('deliveries', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+
+      recipient_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: { model: 'recipients', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+
+      deliveryman_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: { model: 'deliverymen', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+
+      signature_id: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        references: { model: 'signatures', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+
+      product: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      canceled_at: {
+        type: Sequelize.DATE,
+        defaultValue: null,
+      },
+
+      start_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      end_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('deliveries');
+  },
+};
