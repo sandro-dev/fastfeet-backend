243784c26bdfc5600e9efb5c3453d442b417ebae
Sandro Santos <sandro.dev@gmail.com>
2020-02-09 12:20:33 -0300

    new routes to files and render static images

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import authMiddleware from './app/middlewares/auth';
 import checkAdminMiddleware from './app/middlewares/checkAdmin';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import RecipientController from './app/controllers/RecipientController';
+
+const upload = multer(multerConfig);
 
 const routes = new Router();
 
@@ -16,13 +22,14 @@ routes.get('/', (req, res) => {
   });
 });
 
-routes.get('/users', UserController.index);
 routes.post('/sessions', SessionController.store);
+routes.get('/users', UserController.index);
 
 routes.use(authMiddleware);
 
 routes.post('/users', UserController.store);
 routes.put('/users/:id', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
 
 routes.use(checkAdminMiddleware);
 
