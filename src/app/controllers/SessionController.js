0c60447ed5750b42bad021bc59d4e7b1ed726c1d
Sandro Santos <sandro.dev@gmail.com>
2020-03-14 18:17:28 -0300

    Validation on SessionController and attributes on UserController methods index and update

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,14 +1,24 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, password } = req.body;
 
-    const user = await User.findOne({
-      where: { email },
-    });
+    const user = await User.findOne({ where: { email } });
 
     if (!user) {
       return res.status(400).json({ error: 'User not found' });
@@ -18,10 +28,10 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar_id } = user;
 
     return res.json({
-      user: { id, name },
+      user: { id, name, email, avatar_id },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,7 +4,7 @@ import User from '../models/User';
 class UserController {
   async index(req, res) {
     const users = await User.findAll({
-      attributes: ['id', 'name', 'email', 'password_hash'],
+      attributes: ['id', 'name', 'email', 'password_hash', 'avatar_id'],
     });
 
     return res.json(users);
@@ -27,9 +27,7 @@ class UserController {
 
     const { email } = req.body;
 
-    const userExists = await User.findOne({
-      where: { email },
-    });
+    const userExists = await User.findOne({ where: { email } });
 
     if (userExists) {
       return res.status(400).json({ error: 'User already exists' });
@@ -44,7 +42,7 @@ class UserController {
     const schema = Yup.object().shape({
       name: Yup.string(),
       email: Yup.string().email(),
-      password: Yup.string(),
+      password: Yup.string().min(6),
     });
 
     if (!(await schema.isValid(req.body))) {
@@ -56,6 +54,7 @@ class UserController {
 
     const user = await User.findOne({
       where: { id },
+      attributes: ['id', 'name', 'email', 'password_hash', 'avatar_id'],
     });
 
     user.name = name;
@@ -74,9 +73,7 @@ class UserController {
   async delete(req, res) {
     const { id } = req.params;
 
-    const user = await User.destroy({
-      where: { id },
-    });
+    const user = await User.destroy({ where: { id } });
 
     if (!user) {
       return res.json({ error: `Error on try to delete user` });
